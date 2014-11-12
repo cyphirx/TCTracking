b02da4fc87809089e8b623c59101051affb94c5f
Kalrogin <Crixsnrdr@gmail.com>
2014-11-12 07:57:14 -0500

    System still in a non-functional state, hope to have fixed soon. Retrieving more stuff from Flasky

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,36 @@
-from flask import Blueprint
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from config import config
 
-#main = Blueprint('main', __name__)
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
 
-#from . import views, errors
-#from ..models import Permission
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
-#@main.app_context_processor
-#def inject_permissions():
-#    return dict(Permission=Permission)
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+#    from .main import main as main_blueprint
+#    app.register_blueprint(main_blueprint)
+
+#    from .auth import auth as auth_blueprint
+#    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
